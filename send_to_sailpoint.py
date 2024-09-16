6e90b043c34ce924a5b8955f62991d577e155752
hamzaayman216 <85422836+hamzaayman216@users.noreply.github.com>
2024-09-16 17:49:14 +0300

    Update send_to_sailpoint.py

--- send_to_sailpoint.py
+++ send_to_sailpoint.py
@@ -2,45 +2,81 @@ import http.client
 import json
 import os
 
-def get_sailpoint_users(api_url, auth_header):
-    """Retrieves all users from SailPoint."""
+SAILPOINT_API_URL = os.getenv('SAILPOINT_API_URL', '35.170.66.218')
+SAILPOINT_AUTH_HEADER = os.getenv('SAILPOINT_AUTH_HEADER', 'Basic c3BhZG1pbjphZG1pbg==')
+
+def format_user_details(user_name):
+    name_parts = user_name.split(' ')
+    if len(name_parts) > 1:
+        given_name = name_parts[0]
+        family_name = ' '.join(name_parts[1:])
+        display_name = user_name
+    else:
+        given_name = user_name
+        family_name = ''
+        display_name = user_name
+
+    return family_name, given_name, display_name
+
+def send_to_sailpoint(user_name, email, api_url, auth_header):
     conn = http.client.HTTPConnection(api_url, 8080)
-    
+
+    family_name, given_name, display_name = format_user_details(user_name)
+
+    payload = json.dumps({
+        "userName": user_name,
+        "name": {
+            "formatted": display_name,
+            "familyName": family_name,
+            "givenName": given_name
+        },
+        "displayName": display_name,
+        "userType": "employee",
+        "active": True,
+        "password": "string",
+        "emails": [
+            {
+                "type": "work",
+                "value": email,
+                "primary": "true"
+            }
+        ],
+    })
+
     headers = {
+        'Content-Type': 'application/scim+json',
         'Accept': 'application/json',
         'Authorization': auth_header
     }
 
+    conn.request("POST", "/identityiq/scim/v2/Users", payload, headers)
+    res = conn.getresponse()
+    data = res.read()
+
+    return {
+        'statusCode': res.status,
+        'body': data.decode("utf-8")
+    }
+
+def get_sailpoint_usernames(api_url, auth_header):
+    conn = http.client.HTTPConnection(api_url, 8080)
+    headers = {
+        'Accept': 'application/json',
+        'Authorization': auth_header
+    }
     conn.request("GET", "/identityiq/scim/v2/Users", headers=headers)
     res = conn.getresponse()
     data = res.read()
-    
-    if res.status != 200:
-        raise Exception(f"Failed to retrieve users from SailPoint: {res.status}")
-    
-    sailpoint_users = json.loads(data.decode("utf-8")).get("Resources", [])
-    return sailpoint_users
+    response_json = json.loads(data.decode("utf-8"))
+    user_names = [user['userName'] for user in response_json.get('Resources', []) if user['userName'] != 'spadmin']
+    return user_names
 
-def delete_sailpoint_user(user_id, api_url, auth_header):
-    """Deletes a user from SailPoint."""
+def delete_user_from_sailpoint(user_name, api_url, auth_header):
     conn = http.client.HTTPConnection(api_url, 8080)
-    
     headers = {
         'Authorization': auth_header
     }
-
-    conn.request("DELETE", f"/identityiq/scim/v2/Users/{user_id}", headers=headers)
+    conn.request("DELETE", f"/identityiq/scim/v2/Users/{user_name}?lookupByName=true", headers=headers)
     res = conn.getresponse()
-    
-    if res.status != 204:
-        raise Exception(f"Failed to delete user from SailPoint: {res.status}")
-    
-    return True
-
-if __name__ == "__main__":
-    # Test SailPoint user retrieval
-    api_url = os.getenv('SAILPOINT_API_URL', 'localhost') 
-    auth_header = os.getenv('SAILPOINT_AUTH_HEADER', '') 
-
-    users = get_sailpoint_users(api_url, auth_header)
-    print(f"Retrieved users from SailPoint: {users}")
+    data = res.read()
+    return data.decode("utf-8")
